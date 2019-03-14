16bba17f92b666f375851d7e67912901f14faa57
Hongbin Zhang <zhan2385@umn.edu>
2019-03-13 21:49:10 -0600

    Now need to debug

--- main.cpp
+++ main.cpp
@@ -70,59 +70,32 @@ void InitBufferWithSize(int size)
 }
 
 // Cuda Prototypes
-__global__ void GSRBKernel(double* deviceInput, double* deviceOutput);
+__global__ void GSRBKernel(double* phi, double* phi_new, double* rhs, double* alpha, double* beta_i,
+                           double* beta_j, double* beta_k, double* lambda, int color);
 
 // C Prototypes
 int GSRB(brickd *phi, brickd *inbox, brickd *phi_new, brick_list &blist, 
           float *dx, int color);
 int GSRBGenerated(brickd *phi, brickd *inbox, brickd *phi_new, brick_list &blist, 
                   float *dx, int color);
-int GSRBCuda(double* phi, double* phi_new, double* rhs, double* alpha, double* beta_i,
-             double* beta_j, double* beta_k, double* lambda, int color);
+int GSRBCuda();
 
 int main(int argc, char** argv)
 {
     // ------------   CPU Benchmark  ---------------
 
     InitBufferWithSize(grid * sizeof(double));
-    
-    printf("GSRB Bench Starting..\n")
-    clock_t start = clock();
-    
     GSRB(phi, phi_new, rhs, alpha, beta_i, beta_j, beta_k, lambda, color);
-    
-    clock_t end = clock();
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("CPU Time is %f\n", time_spent);
-
 
     // ------------   CUDA Benchmark  ---------------
     InitBufferWithSize(grid * sizeof(double));
-    
-    // Timing using cudaEvent
-    cudaEvent_t start, stop;
-    float et;
-    cudaCheck(cudaEventCreate(&start));
-    cudaCheck(cudaEventCreate(&stop));
-    cudaCheck(cudaEventRecord(start));
-
     GSRBCuda(phi, phi_new, rhs, alpha, beta_i, beta_j, beta_k, lambda, color);
-    
-    // Time event end
-    cudaCheck(cudaEventRecord(stop));
-    cudaCheck(cudaEventSynchronize(stop));
-    cudaCheck(cudaEventElapsedTime(&et, start, stop));
-    cudaCheck(cudaEventDestroy(start));
-    cudaCheck(cudaEventDestroy(stop));
 
-    printf("Cuda Time is %f\n", et);
-    
 
     // ------------   BRICKS Benchmark  ---------------
     InitBufferWithSize(grid * sizeof(double));
     // Start Timer
     // TODO: How to time bricks?
-
     GSRBBricks(phi, phi_new, rhs, alpha, beta_i, beta_j, beta_k, lambda, color);
     // End Timer
 }
@@ -132,19 +105,23 @@ void GSRB(double *phi, double *phi_new, double *rhs, double *alpha,
 {
     int i, j, k;
     double h2inv = 1.0/64;
+
+    printf("GSRB Bench Starting..\n")
+    clock_t start = clock();
+
     for(k=0;k<pencil;k++){
       for(j=0;j<pencil;j++){
         for(i=0;i<pencil;i++){
           int ijk = i + j*pencil + k*plane;
           if(i+j+k+color % 2 == 0){ // color signifies red or black case
             double helmholtz = alpha[ijk]*phi[ijk]
-                              -h2inv*(
-                                  beta_i[ijk+1     ]*( phi[ijk+1     ]-phi[ijk       ] )
-                                  -beta_i[ijk       ]*( phi[ijk       ]-phi[ijk-1     ] )
-                                  +beta_j[ijk+pencil]*( phi[ijk+pencil]-phi[ijk       ] )
-                                  -beta_j[ijk       ]*( phi[ijk       ]-phi[ijk-pencil] )
-                                  +beta_k[ijk+plane ]*( phi[ijk+plane ]-phi[ijk       ] )
-                                  -beta_k[ijk       ]*( phi[ijk       ]-phi[ijk-plane ] )
+                             - h2inv*(
+                                 beta_i[ijk+1     ]*( phi[ijk+1     ]-phi[ijk       ] )
+                               - beta_i[ijk       ]*( phi[ijk       ]-phi[ijk-1     ] )
+                               + beta_j[ijk+pencil]*( phi[ijk+pencil]-phi[ijk       ] )
+                               - beta_j[ijk       ]*( phi[ijk       ]-phi[ijk-pencil] )
+                               + beta_k[ijk+plane ]*( phi[ijk+plane ]-phi[ijk       ] )
+                               - beta_k[ijk       ]*( phi[ijk       ]-phi[ijk-plane ] )
                                   );
 
             phi_new[ijk] = phi[ijk] - lambda[ijk]*(helmholtz-rhs[ijk]);
@@ -152,35 +129,47 @@ void GSRB(double *phi, double *phi_new, double *rhs, double *alpha,
         }
       }
     }
+
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("CPU Time is %f\n", time_spent);
 }
 
-
-
-int GSRBCuda(double* phi, double* phi_new, double* rhs, double* alpha, double* beta_i,
-             double* beta_j, double* beta_k, double* lambda, int color)
+int GSRBCuda()
 {
+    //CUDA Buffers
+    double* phi_device    ;
+    double* phi_new_device;
+    double* rhs_device    ;
+    double* alpha_device  ;
+    double* beta_i_device ;
+    double* beta_j_device ;
+    double* beta_k_device ;
+    double* lambda_device ;
 
     // Init Memory on GPU
     // Cuda Memory Management
-    cudaCheck(cudaMalloc((void**) &device_input, inputSize));
-    // cudaCheck(cudaGetLastError());
-    cudaCheck(cudaMalloc((void**) &device_output, inputSize));
-    // cudaCheck(cudaGetLastError());
-    cudaCheck(cudaMemcpy(device_input, host_input, inputSize, cudaMemcpyHostToDevice));
-    // cudaCheck(cudaGetLastError());
-    cudaCheck(cudaMemcpy(device_output, host_output, inputSize, cudaMemcpyHostToDevice));
-    // cudaCheck(cudaGetLastError());
+    cudaCheck(cudaMalloc((void**) &phi_device    , grid * sizeof(double)));
+    cudaCheck(cudaMalloc((void**) &phi_new_device, grid * sizeof(double)));
+    cudaCheck(cudaMalloc((void**) &rhs_device    , grid * sizeof(double)));
+    cudaCheck(cudaMalloc((void**) &alpha_device  , grid * sizeof(double)));
+    cudaCheck(cudaMalloc((void**) &beta_i_device , grid * sizeof(double)));
+    cudaCheck(cudaMalloc((void**) &beta_j_device , grid * sizeof(double)));
+    cudaCheck(cudaMalloc((void**) &beta_k_device , grid * sizeof(double)));
+    cudaCheck(cudaMalloc((void**) &lambda_device , grid * sizeof(double)));
+    cudaCheck(cudaGetLastError());
 
-    /// Timer
-    // Timing using cudaEvent
-    cudaEvent_t start, stop;
-    float et;
-    cudaCheck(cudaEventCreate(&start));
-    cudaCheck(cudaEventCreate(&stop));
+    cudaCheck(cudaMemcpy(phi_device    , phi    , grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(phi_new_device, phi_new, grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(rhs_device    , rhs    , grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(alpha_device  , alpha  , grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(beta_i_device , beta_i , grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(beta_j_device , beta_j , grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(beta_k_device , beta_k , grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(lambda_device , lambda , grid * sizeof(double), cudaMemcpyHostToDevice));
+    cudaCheck(cudaGetLastError());
 
-    // Time event start
-    cudaCheck(cudaEventRecord(start));
-
+    // Printing out some device data
     struct cudaDeviceProp properties;
     cudaGetDeviceProperties(&properties, 0);
     int maxGridSize = properties.maxGridSize[0];
@@ -191,11 +180,6 @@ int GSRBCuda(double* phi, double* phi_new, double* rhs, double* alpha, double* b
             maxGridSize,    maxBlockSize,    maxThreadCount,       sharedMemoryPerBlock);
 
     // Dimension
-
-    // Baseline
-    // long numOfBlocks = (width*height) / BLOCKSIZE + 1;
-
-    // 
     long numOfBlocks = (width*height) / (BLOCKSIZE * TILESIZE) + 1;
 
     dim3 dimGrid(numOfBlocks);
@@ -205,15 +189,20 @@ int GSRBCuda(double* phi, double* phi_new, double* rhs, double* alpha, double* b
     printf("Arguments: threshold %d, width %d, height %d, inputSize %d\n", threshold, width, height, inputSize);
     printf("input address %p, output address %p\n", (void*)device_input, (void*)device_output);
 
-    // SobelKernel<<<dimGrid, dimBlock>>>(device_input, device_output, 
-    //                                    threshold, width, height, width*height);
+    cudaEvent_t start, stop;
+    float et;
+    cudaCheck(cudaEventCreate(&start));
+    cudaCheck(cudaEventCreate(&stop));
+    cudaCheck(cudaEventRecord(start));
 
-    // TiledSobelKernel<<<dimGrid, dimBlock>>>(device_input, device_output, 
-                                            // threshold, width, height, width*height);
+    // Cuda Kernel Call
+    GSRBKernel<<<dimGrid, dimBlock>>> (phi_device, phi_new_device, rhs_device, alpha_device, beta_i_device, 
+                                       beta_j_device , beta_k_device , lambda_device, color);
 
     cudaCheck(cudaGetLastError());
     cudaDeviceSynchronize();
     cudaCheck(cudaGetLastError());
+
     // Time event end
     cudaCheck(cudaEventRecord(stop));
     cudaCheck(cudaEventSynchronize(stop));
@@ -221,7 +210,7 @@ int GSRBCuda(double* phi, double* phi_new, double* rhs, double* alpha, double* b
     cudaCheck(cudaEventDestroy(start));
     cudaCheck(cudaEventDestroy(stop));
 
-    printf("\t%0.3f\n", et);
+    printf("Cuda Time is %f\n", et);
 
     // More Memory Management
     cudaMemcpy(host_output, device_output, inputSize, cudaMemcpyDeviceToHost);
@@ -229,15 +218,28 @@ int GSRBCuda(double* phi, double* phi_new, double* rhs, double* alpha, double* b
     cudaFree(device_input);
 }
 
-// Cuda Stuff
-
-__global__ void GSRBKernel(double* deviceInput, double* deviceOutput)
+__global__ void GSRBKernel(double* phi, double* phi_new, double* rhs, double* alpha, double* beta_i,
+                           double* beta_j, double* beta_k, double* lambda, int color)
 {
-    // i, j, k
-
+    // TODO: Find out what i, j, k maps to in terms of blockIdx, blockDim, threadIdx
+    // int currentOffset = blockIdx.x + blockDim.x + threadIdx.x;
     
+    int ijk = i + j*pencil + k*plane;
 
+    if (i+j+k+color % 2 == 0)
+    {
+        double helmholtz = alpha[ijk]*phi[ijk]
+                         - h2inv*(
+                               beta_i[ijk+1     ]*( phi[ijk+1     ]-phi[ijk       ] )
+                             - beta_i[ijk       ]*( phi[ijk       ]-phi[ijk-1     ] )
+                             + beta_j[ijk+pencil]*( phi[ijk+pencil]-phi[ijk       ] )
+                             - beta_j[ijk       ]*( phi[ijk       ]-phi[ijk-pencil] )
+                             + beta_k[ijk+plane ]*( phi[ijk+plane ]-phi[ijk       ] )
+                             - beta_k[ijk       ]*( phi[ijk       ]-phi[ijk-plane ] )
+                              );
 
+        phi_new[ijk] = phi[ijk] - lambda[ijk]*(helmholtz-rhs[ijk]);
+    }
 }
 
 
