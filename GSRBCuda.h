20dae981d9dfa37437dfa0767ee974b4d3bda529
Hongbin Zhang <u1143081@notchpeak1.int.chpc.utah.edu>
2019-03-18 10:53:48 -0600

    Now Compiling

--- GSRBCuda.h
+++ GSRBCuda.h
@@ -1,6 +1,8 @@
 #ifndef GSRBCuda_h
 #define GSRBCuda_h
 
+#include <stdio.h>
+
 void GSRBCuda(double* phi, double* phi_new, double* rhs, double* alpha, double* beta_i,
               double* beta_j, double* beta_k, double* lambda);
 
